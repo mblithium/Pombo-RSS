8cf305f15ebe0847864e8f3e9f14e3b6721bab5c
Matheus Bastos <matheusmcy1@gmail.com>
2025-02-09 23:44:14 -0300

    Adiciona configuração para geração de ativos PWA e ícones temporários

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,37 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const manifest = {
+  "name": 'Pombo RSS',
+  "short_name": 'Pombo RSS',
+  "description": 'Leia suas notícias em qualquer lugar',
+  "theme_color": '#ffffff',
+  "icons": [
+    {
+      "src": "pwa-64x64.png",
+      "sizes": "64x64",
+      "type": "image/png"
+    },
+    {
+      "src": "pwa-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "pwa-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "maskable-icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Pombo-RSS/',
